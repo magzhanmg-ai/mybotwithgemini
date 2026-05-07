846b13d8515b2f6ea74f3443265a29d4e41a91d2
magzhanmg-ai <magzhan.mg@gmail.com>
2026-05-07 11:09:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 logging.basicConfig(level=logging.INFO)
 
 # Ключи и настройки
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-GEMINI_KEY = os.getenv('GEMINI_API_KEY')
+BOT_TOKEN = "8643907201:AAFsUqu288MfVlDwk_WoS2TP60wwzCmD5ug"
+GEMINI_KEY = "AIzaSyC9wh_8AJyWVfztPQ_m1VhzoUBT0BgPPGU"
 SHEET_NAME = os.getenv('SHEET_NAME', 'Финансы')
 
 # Настройка Gemini
